f4b41838c9c5da29476591a9882fa69be0a0b560
Victor Lascares <vmlascares@gmail.com>
2022-06-30 13:10:14 -0500

    Agregando formulario con Formik

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -8,19 +8,19 @@ const Layout = () => {
         <div className="md:flex md:min-h-screen">
             <div className="md:w-1/4 md:bg-gradient-to-b bg-purple-900 px-5 py-1">
                 <h2 className="text-center text-4xl text-white font-bold mt-4">CRM - Clientes</h2>
-                <nav className="mt-10">
+                <nav className="mt-10 flex md:flex-col justify-center md:items-start items-center gap-4">
                     <Link 
-                        className={`${currentUrl === '/clients' ? 'text-purple-300': 'text-white'} text-white text-2xl block mt-2 hover:text-purple-300`}
+                        className={`${currentUrl === '/clients' ? 'text-purple-300': 'text-white'} text-white text-2xl mt-2 hover:text-purple-300 hover:cursor-pointer`}
                         to="/clients"
                     >Clientes</Link>
                     <Link 
-                        className={`${currentUrl === '/clients/new' ? 'text-purple-300': 'text-white'} text-white text-2xl block mt-2 hover:text-purple-300`}
+                        className={`${currentUrl === '/clients/new' ? 'text-purple-300': 'text-white'} text-white text-2xl mt-2 hover:text-purple-300 hover:cursor-pointer`}
                         to="/clients/new">Nuevo Cliente</Link>
                 </nav>
             </div>
-            <div className="md:w-3/4">
+            <div className="md:w-3/4 p-10 bg-gray-100">
                 <Outlet />
-            </div>
+            </div> 
 
         </div>
     )
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import FormClient from '../components/FormClient'
 
 const Create = () => {
     return (
-        <div>
-            <h1>Nuevo Cliente</h1>
-        </div>
+        <>
+            <h1 className='font-black text-4xl text-purple-900'>Nuevo Cliente</h1>
+            <p className='mt-3 text-gray-500'>Llena los siguientes campos para registrar a un cliente</p>
+
+            <FormClient />
+        </>
     )
 }
 
