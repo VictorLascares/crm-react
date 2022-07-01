19b801be971387c4197de2fa758030d479670e39
Victor Lascares <vmlascares@gmail.com>
2022-07-01 12:20:58 -0500

    Solucionando el problemas de carga del componente

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 const Show = () => {
     const [client, setClient] = useState({})
+    const [charging, setCharging] = useState(false)
     const params = useParams()
 
     const getClient = async () => {
@@ -17,36 +18,42 @@ const Show = () => {
     }
     
     useEffect(() => {
+        setCharging(!charging);
         getClient();
+        setCharging(false);
     }, [])
     
 
     return (
         <div>
-            <h1 className="text-4xl text-gray-700">
-                <span className="uppercase font-bold">Cliente: </span>
-                {client.name}
-            </h1>
-            <p className="mt-10 text-xl text-gray-500">Información del cliente</p>
-            <p className="text-xl text-gray-700 mt-4">
-                <span className="uppercase font-bold">Correo Eléctronico: </span>
-                {client.email}
-            </p>
-            {client.phone && (
-                <p className="text-xl text-gray-700 mt-4">
-                    <span className="uppercase font-bold">Teléfono: </span>
-                    {client.phone}
-                </p>
-            )}
-            <p className="text-xl text-gray-700 mt-4">
-                <span className="uppercase font-bold">Empresa: </span>
-                {client.business}
-            </p>
-            {client.notes && (
-                <p className="text-xl text-gray-700 mt-4">
-                    <span className="uppercase font-bold">Notas: </span>
-                    {client.notes}
-                </p>
+            {charging ? 'cargando...': (
+                <>
+                    <h1 className="text-4xl text-gray-700">
+                        <span className="uppercase font-bold">Cliente: </span>
+                        {client.name}
+                    </h1>
+                    <p className="mt-10 text-xl text-gray-500">Información del cliente</p>
+                    <p className="text-xl text-gray-700 mt-4">
+                        <span className="uppercase font-bold">Correo Eléctronico: </span>
+                        {client.email}
+                    </p>
+                    {client.phone && (
+                        <p className="text-xl text-gray-700 mt-4">
+                            <span className="uppercase font-bold">Teléfono: </span>
+                            {client.phone}
+                        </p>
+                    )}
+                    <p className="text-xl text-gray-700 mt-4">
+                        <span className="uppercase font-bold">Empresa: </span>
+                        {client.business}
+                    </p>
+                    {client.notes && (
+                        <p className="text-xl text-gray-700 mt-4">
+                            <span className="uppercase font-bold">Notas: </span>
+                            {client.notes}
+                        </p>
+                    )}
+                </>
             )}
         </div>
     )
