7ef39599805e242639fd59342a865a826d0400d0
Victor Lascares <vmlascares@gmail.com>
2022-06-30 19:48:30 -0500

    Consultando la API para obtener los clientes

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import { useEffect, useState } from "react"
 
 const Index = () => {
+    const [clients, setClients] = useState([])
+
+    const getClients = async () => {
+        try {
+            const url = 'http://localhost:4000/clients';
+            const response = await fetch(url);
+            const result = await response.json();
+            setClients(result);
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getClients()
+    }, [])
+    
+
+
     return (
         <div>
             <h1>Inicio</h1>
