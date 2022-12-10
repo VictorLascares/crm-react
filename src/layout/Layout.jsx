f5f2a09fbc2c884c8d47d660b0152f8b4ff3f5bf
VictorLascares <vmlascares@gmail.com>
2022-12-10 12:00:41 -0600

    Actualizando layout y el Routing

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -6,21 +6,21 @@ const Layout = () => {
 
     return (
         <div className="md:flex md:min-h-screen">
-            <div className="md:w-1/4 md:bg-gradient-to-b bg-purple-900 px-5 py-1">
-                <h2 className="text-center text-4xl text-white font-bold mt-4">CRM - Clientes</h2>
+            <aside className="md:w-1/4 bg-purple-900 px-5 py-1">
+                <h1 className="text-center text-4xl text-white font-bold mt-4">CRM - Clientes</h1>
                 <nav className="mt-10 pb-5 md:pb-0 flex md:flex-col justify-center md:items-start items-center gap-4">
                     <Link 
-                        className={`${currentUrl === '/clients' ? 'text-purple-300': 'text-white'} text-white text-2xl mt-2 hover:text-purple-300 hover:cursor-pointer`}
-                        to="/clients"
+                        className={`${currentUrl === '/' ? 'text-purple-300': 'text-white'} text-white text-2xl mt-2 hover:text-purple-300 hover:cursor-pointer`}
+                        to="/"
                     >Clientes</Link>
                     <Link 
                         className={`${currentUrl === '/clients/new' ? 'text-purple-300': 'text-white'} text-white text-2xl mt-2 hover:text-purple-300 hover:cursor-pointer`}
                         to="/clients/new">Nuevo Cliente</Link>
                 </nav>
-            </div>
-            <div className="md:w-3/4 py-10 px-5">
+            </aside>
+            <main className="md:w-3/4 py-10 px-5">
                 <Outlet />
-            </div> 
+            </main> 
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Index from "./pages/Index";
+import Create from "./pages/Create";
+import Edit from "./pages/Edit";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+      },
+      {
+        path: "/clients/new",
+        element: <Create />,
+      },
+      {
+        path: "/clients/edit/:id",
+        element: <Edit />,
+      },
+      {
+        path: "/clients/:id",
+        element: <Edit />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
