031943a5bddd3625b5e1daf18ed0e3b920444fe5
VictorLascares <vmlascares@gmail.com>
2022-12-10 16:55:25 -0600

    Obteniendo la informacion ingresada a un formulario con formData

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,18 +1,22 @@
-import FormClient from '../components/FormClient'
+import FormClient from "../components/FormClient";
 
-export function action() {
-  console.log("Submit al formulario");
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  console.log(data);
 }
 
 const Create = () => {
-    return (
-        <>
-            <h1 className='font-black text-4xl text-purple-900'>Nuevo Cliente</h1>
-            <p className='mt-3 text-gray-500'>Llena los siguientes campos para registrar a un cliente</p>
+  return (
+    <>
+      <h1 className="font-black text-4xl text-purple-900">Nuevo Cliente</h1>
+      <p className="mt-3 text-gray-500">
+        Llena los siguientes campos para registrar a un cliente
+      </p>
 
-            <FormClient />
-        </>
-    )
-}
+      <FormClient />
+    </>
+  );
+};
 
-export default Create
+export default Create;
